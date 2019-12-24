a1337d1d500449a8423cb6a0f20c3e74e79c7b36
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-12-23 20:07:36 -0700

    hopefully finished main home page content

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Improved eCampus
+# Improved eCampus - Violentmonkey User Script
 
 The goal is to improve on the layout and UI of eCampus so that it is more pleasurable to use.
--- styles-content.js
+++ styles-content.js
@@ -16,7 +16,7 @@ async function getCourseOrgObjects(serviceLevel) {
     let courseResponse = await fetch(`https://tamu.blackboard.com/webapps/blackboard/execute/globalCourseNavMenuSection?cmd=view&serviceLevel=blackboard.data.course.Course$ServiceLevel:${serviceLevel}`, {
         "credentials": "include",
         "headers": {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0",
+            "User-Agent": navigator.userAgent,
             "Accept": "text/javascript, text/html, application/xml, text/xml, */*",
             "Accept-Language": "en-US,en;q=0.5",
             "X-Requested-With": "XMLHttpRequest",
@@ -42,20 +42,81 @@ async function getCourseOrgObjects(serviceLevel) {
     }})))
 }
 
+async function getUpdateAndGradeCount() {
+    let response = await fetch("https://tamu.blackboard.com/webapps/portal/dwr_open/call/plaincall/ToolActivityService.getActivityForAllTools.dwr", {
+        "credentials": "include",
+        "headers": {
+            "User-Agent": navigator.userAgent,
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Suppress-Session-Timestamp-Update": "true",
+            "Content-Type": "text/plain"
+        },
+        "referrer": "https://tamu.blackboard.com/webapps/portal/execute/tabs/tabAction?tabId=_4190_1&tab_tab_group_id=_25_1",
+        "body": "callCount=1\npage=/webapps/portal/execute/tabs/tabAction?tabId=_4190_1&tab_tab_group_id=_25_1\nhttpSessionId=D6EEDA8A6EB132762462DBCE31E1682A\nscriptSessionId=8A22AEE4C7B3F9CA3A094735175A6B14350\nc0-scriptName=ToolActivityService\nc0-methodName=getActivityForAllTools\nc0-id=0\nc0-param0=boolean:true\nbatchId=0\n",
+        "method": "POST",
+        "mode": "cors"
+    })
 
-async function makeStyles(stylesOn) {
-    if (stylesOn) {
-        
-        let courses = await getCourseOrgObjects('FULL')
-        let organizations = await getCourseOrgObjects('COMMUNITY')
-
-        let name = document.getElementById('global-nav-link').childNodes[1].nodeValue.trim()
-
-        // Remove listeners
-
-        // Remove header content. Replace with name
-
-    }
+    let objectString = await response.text()
+    objectString = /\{.*\}/g.exec(objectString)[0]
+    let jsonStr = objectString.replace(new RegExp("'", 'g'), '"')
+    
+    return JSON.parse(jsonStr)
 }
 
-browser.storage.local.get('style').then(makeStyles, onError)
+async function runAsyncActions() {
+
+    const updatesKey = 'AlertsOnMyBb_____AlertsTool'
+    const gradesKey = 'MyGradesOnMyBb_____MyGradesTool'
+    
+    getCourseOrgObjects('FULL').then(courses => {
+        //  Add courses to dropdown
+
+    }).catch(onError)
+
+    getCourseOrgObjects('COMMUNITY').then(orgs => {
+        // Add orgs to to dropdown
+
+    }).catch(onError)
+
+    getUpdateAndGradeCount().then(counts => {
+        // Add counts to notifications and grade links
+
+    }).catch(onError)
+
+}
+
+function main() {
+    let name = document.getElementById('global-nav-link').childNodes[1].nodeValue.trim()
+    let homeURL = document.getElementById('eCampus').children[0].baseURI
+
+    // let gradeAtag = document.getElementById('MyGradesOnMyBb_____MyGradesTool')
+    let gradeURL = '/webapps/bb-social-learning-BBLEARN/execute/mybb?cmd=display&toolId=MyGradesOnMyBb_____MyGradesTool'
+    
+    let updateAmount = document.getElementById('badgeTotalCount') // Number of updates
+    let updateURL = '/webapps/bb-social-learning-BBLEARN/execute/mybb?cmd=display&toolId=AlertsOnMyBb_____AlertsTool'
+
+    let logoutAtag = document.getElementById('topframe.logout.label')
+    
+    runAsyncActions()
+
+    //  Remove Tools and Report Card
+    
+
+    let coursePanel = document.getElementById('div_4_1')
+    let orgPanel = document.getElementById('div_5_1')
+    let announcementPanel = document.getElementById('div_1_1')
+
+    // GM_addStyle('#column0 { display: none; }')
+    GM_addStyle('.locationPane { display: none; }')
+
+    let dashboard = document.createElement('div')
+    dashboard.appendChild(coursePanel)
+    dashboard.appendChild(orgPanel)
+    dashboard.appendChild(announcementPanel)
+
+    document.getElementById('globalNavPageContentArea').appendChild(dashboard)
+}
+
+main()
