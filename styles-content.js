de74fff8ff732dac6c09be31364743057c0e0139
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2019-12-23 21:15:51 -0700

    updated version

--- styles-content.js
+++ styles-content.js
@@ -2,7 +2,7 @@
 // @name         Blackboard: simplification
 // @namespace    http://tmcd.me/
 // @downloadURL  https://raw.githubusercontent.com/tmcdo1/improved-ecampus-extension/master/styles-content.js
-// @version      0.51
+// @version      0.52
 // @description  Remove excessive content
 // @author       Thomas McDonald
 // @match        https://tamu.blackboard.com/webapps/*
